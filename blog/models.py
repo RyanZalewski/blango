ef69a7ef67c633dd63700259cfb368ad8868f65b
Ryan Zalewski <ryan.zalewski@a1fed.com>
2023-04-25 19:48:04 +0000

    finish custom user model

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
-
+from django.utils.translation import gettext_lazy as _
 class Tag(models.Model):
    value = models.TextField(max_length=100)
 
    def __str__(self):
      return self.value
+
+class AuthorProfile(models.Model):
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+  bio = models.TextField()
+
+  def __str__(self):
+    return f"{self.__class__.__name__} object for {self.user}"
 
 class Comment(models.Model):
   creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
